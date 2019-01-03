d730388d21bb49b88140c11cc4eadb19c934a2d0
MaksimVRS <social.maksim.vrs@gmail.com>
2019-01-03 19:57:44 +0500

    Fix logging

--- src/gui/__main__.py
+++ src/gui/__main__.py
@@ -2,11 +2,8 @@ import sys
 
 from PyQt5 import QtWidgets
 
-import logging
-
 from src.gui.client_gui import ClientGUI
 
-logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
